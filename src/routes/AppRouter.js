a133ca9e45674f2aa53e2a7b677352d9e33fcf3e
Anis Merchant <anisxmerchant@gmail.com>
2019-05-14 03:12:01 -0400

    refactor: implement login authentication

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { LoginPage } from '../components/LoginPage';
+import LoginPage from '../components/LoginPage';
 
 const AppRouter = () => (
     <BrowserRouter>
