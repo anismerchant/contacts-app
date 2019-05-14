be66f515d1554f822400e91b9821cdee02582fcd
Anis Merchant <anisxmerchant@gmail.com>
2019-05-14 13:41:22 -0400

    feat: implement firebase logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = () => (
+export const Header = ({startLogout}) => (
     <header>
         <h1> Contacts App</h1>
-        <button>Logout</button>
+        <button onClick={startLogout}>Logout</button>
     </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
