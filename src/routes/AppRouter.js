15b75947cf6cde31bb085d83f8e9627ea02164d6
Anis Merchant <anisxmerchant@gmail.com>
2019-05-14 01:32:01 -0400

    setup default login page route

---
+++ src/routes/AppRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { LoginPage } from '../components/LoginPage';
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Switch> 
+                <Route exact path= '/' component={LoginPage} />
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter; 
