c0eb8310dfb5a5de3ef62e8665395d584b87c4f1
Anis Merchant <anisxmerchant@gmail.com>
2019-05-14 02:49:14 -0400

    implement app-wide access to redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import AppRouter from './routes/AppRouter';
 import * as serviceWorker from './serviceWorker';
+import { firebase } from './firebase/firebase';
+import './index.css';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const store = configureStore;
+
+const jsx = (
+    <Provider store = {store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
