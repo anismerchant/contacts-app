a576d0128abbacbcd5cdc27fbaaa9bf52a2a9b87
Anis Merchant <anisxmerchant@gmail.com>
2019-05-14 14:59:23 -0400

    test: implement logout test

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -6,3 +6,10 @@ test('It should correctly render Header.', () => {
     const wrapper = shallow(<Header startLogout={() => { }} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('It should call startLogout on button click.', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />)
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
+});
