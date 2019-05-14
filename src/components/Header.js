9351af53566c4a222bdad9fad69d95dcd3b0eb60
Anis Merchant <anisxmerchant@gmail.com>
2019-05-14 13:12:52 -0400

    create Header component with logout button

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1> Contacts App</h1>
+        <button>Logout</button>
+    </header>
+);
+
+export default Header;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
+            <Header />
             <Switch> 
                 <Route exact path= '/' component={LoginPage} />
             </Switch>
