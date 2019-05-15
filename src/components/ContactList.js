082f0fa0ddce850e1e7d4fe3c611c3e4763f07dd
Anis Merchant <anisxmerchant@gmail.com>
2019-05-15 19:38:55 -0400

    feat: connect 'ContactList' to store with search functionality wired up

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ContactListPerson from './ContactListPerson';
+import findContacts from '../selectors/findContacts';
 
-const ContactList = () => (
+// de-structured props
+const ContactList = ({ contacts }) => (
     <div>
         <h2>Contact List</h2>
+        {contacts.length}
+        {contacts.map( (contact) => <ContactListPerson key={contact.id} {...contact} />)}
     </div>
 );
 
-export default ContactList;
+const mapStateToProps = (state) => {
+    return {
+        contacts: findContacts(state.contacts, state.filters)
+    }
+}
+
+export default connect(mapStateToProps)(ContactList); 
