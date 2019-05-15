6ff97a0a3c9aa1b4264e87896b44052f370804b1
Anis Merchant <anisxmerchant@gmail.com>
2019-05-15 04:06:20 -0400

    feat: implement sort by first or last name

--- src/selectors/findContacts.js
+++ src/selectors/findContacts.js
@@ -1,8 +1,17 @@
 export default (contacts, {name, email, cell, sortBy}) => {
     return contacts.filter( contact => {
+        // implement search filter for only firstname and lastname
         const firstnameMatch = contact.firstname.toLowerCase().includes(name.toLowerCase());
         const lastnameMatch = contact.lastname.toLowerCase().includes(name.toLowerCase());
-        
         return firstnameMatch || lastnameMatch;
+    }).sort ( (a,b) => {
+        let sortName;
+        if (sortBy === 'firstname') {
+            // sort by ascending order
+            sortName = a.firstname < b.firstname ? -1 : 1; 
+        } else if (sortBy === 'lastname') {
+            sortName = a.lastname < b.lastname ? -1 : 1;
+        }
+        return sortName;
     });
-}
+};
