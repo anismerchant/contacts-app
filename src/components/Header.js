3f0359bd5f6946bb09c9441d872a5edf363158b2
Anis Merchant <anisxmerchant@gmail.com>
2019-05-15 19:40:59 -0400

    refactor: add 'Home' to nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ import { startLogout } from '../actions/auth';
 export const Header = ({startLogout}) => (
     <header>
         <h1> Contacts App</h1>
-        <NavLink exact to='/'>Dashboard</NavLink>
+        <NavLink exact to='/'>Home</NavLink>
+        <NavLink exact to='/dashboard'>Dashboard</NavLink>
         <NavLink to='/add'>Add Contact</NavLink>
         <NavLink to='/feedback'>Feedback</NavLink>
         <button onClick={startLogout}>Logout</button>
