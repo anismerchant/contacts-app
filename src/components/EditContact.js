5b88ffda575b340029649bdad24746ccfe8d8ea3
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 13:15:13 -0400

    feat: implement UI to edit contact details from list

--- src/components/EditContact.js
+++ src/components/EditContact.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { editContact } from '../actions/contacts';
+import ContactForm from './ContactForm';
 
-const EditContact = () => (
-    <div>
-        Edit Contact Page
-    </div>
-);
+export class EditContact extends Component {
+    onSubmit = (contact) => {
+        this.props.editContact(this.props.contact.id, contact);
+        this.props.history.push('/dashboard');
+    }
+    render() {
+            return (
+                <div>
+                    <ContactForm 
+                        contact={this.props.contact}
+                        onSubmit={this.onSubmit}
+                    />
+                </div>
+        );
+    }
+};
 
-export default EditContact;
+const mapStateToProps = (state, props) => ({
+    contact: state.contacts.find(contact => contact.id === props.match.params.id)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    editContact: (id, contact) => dispatch(editContact(id, contact))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditContact);
