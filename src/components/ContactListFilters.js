df3b28897139dc9552f77424b6798d71b41be7a1
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 13:12:33 -0400

    refactor: change conditinal syntax and include 'Sort by' label

--- src/components/ContactListFilters.js
+++ src/components/ContactListFilters.js
@@ -6,17 +6,22 @@ const ContactListFilters = (props) => (
     <div>
         <input 
             type="text" 
+            placeholder="Search"
             value={props.filters.name} 
             onChange={(e) => props.dispatch(setNameFilter(e.target.value))}
         />
         <select
             value={props.filters.sortBy}
             onChange = {(e) => 
-            {e.target.value === "firstname" 
-            ? props.dispatch(sortByFirstName())
-            : props.dispatch(sortByLastName())
+            {
+                if (e.target.value === "firstname") {
+                    props.dispatch(sortByFirstName())
+                } else if (e.target.value === "lastname") {
+                    props.dispatch(sortByLastName())
+                } 
             }}
-        >
+        >   
+            <option value="" disabled>Sort by</option>
             <option value="firstname">First Name</option>
             <option value="lastname">Last Name</option>
         </select>
