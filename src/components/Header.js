5f11c5a3c112222410ab8af8f758e25e5e1f5b89
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 12:49:51 -0400

    fix: remove exact attribute from 'dashboard' nav link

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = ({startLogout}) => (
     <header>
         <h1> Contacts App</h1>
         <NavLink exact to='/'>Home</NavLink>
-        <NavLink exact to='/dashboard'>Dashboard</NavLink>
+        <NavLink to='/dashboard'>Dashboard</NavLink>
         <NavLink to='/add'>Add Contact</NavLink>
         <NavLink to='/feedback'>Feedback</NavLink>
         <button onClick={startLogout}>Logout</button>
