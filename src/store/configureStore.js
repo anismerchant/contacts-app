e27d71b6f20bddbf405fd56d0ca2520650edce08
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 01:35:29 -0400

    fix: commit missing filters' reducer in store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose  } from 'redux';
 import contactsReducer from '../reducers/contacts';
+import filtersReducer from '../reducers/filters';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(combineReducers({
-        contacts: contactsReducer
+        contacts: contactsReducer,
+        filters: filtersReducer
     }),
     composeEnhancers(applyMiddleware(thunk)),
 );
