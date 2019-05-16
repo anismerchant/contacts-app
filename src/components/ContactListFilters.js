4a51462ddd7f84df8b1eef2861dc041a09b1c4ac
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 01:39:43 -0400

    feat: implement sort by UI functionality

--- src/components/ContactListFilters.js
+++ src/components/ContactListFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setNameFilter } from '../actions/filters';
+import { setNameFilter, sortByFirstName, sortByLastName } from '../actions/filters';
 
 const ContactListFilters = (props) => (
     <div>
@@ -9,6 +9,17 @@ const ContactListFilters = (props) => (
             value={props.filters.name} 
             onChange={(e) => props.dispatch(setNameFilter(e.target.value))}
         />
+        <select
+            value={props.filters.sortBy}
+            onChange = {(e) => 
+            {e.target.value === "firstname" 
+            ? props.dispatch(sortByFirstName())
+            : props.dispatch(sortByLastName())
+            }}
+        >
+            <option value="firstname">First Name</option>
+            <option value="lastname">Last Name</option>
+        </select>
     </div>
 
 );
