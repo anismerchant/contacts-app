a749ef9a357e4cd38df558b26b86521c14253852
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 13:16:33 -0400

    feat: add edit button

--- src/components/ContactListPerson.js
+++ src/components/ContactListPerson.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { removeContact } from '../actions/contacts';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 // de-structured props
 const ContactListPerson = ({dispatch, id, firstname, lastname, email, cell}) => (
     <div>
         <p>{`${firstname} ${lastname} ${email} ${cell}`}</p>
+        <Link to={`/edit/${id}`}><div>Edit</div></Link>
         <button onClick={ () => {dispatch(removeContact({id}))
         }}>Remove</button>
     </div>
