321f96630ddad809c4dd2ef14312e78b9437bd92
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 01:40:53 -0400

    feat: implement UI to remove contact(s) from list

--- src/components/ContactListPerson.js
+++ src/components/ContactListPerson.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { removeContact } from '../actions/contacts';
+import { connect } from 'react-redux';
 
 // de-structured props
-const ContactListPerson = ({firstname, lastname, email, cell}) => (
+const ContactListPerson = ({dispatch, id, firstname, lastname, email, cell}) => (
     <div>
         <p>{`${firstname} ${lastname} ${email} ${cell}`}</p>
+        <button onClick={ () => {dispatch(removeContact({id}))
+        }}>Remove</button>
     </div>
 );
 
-export default ContactListPerson;
+export default connect()(ContactListPerson);
