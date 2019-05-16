bdb31275aac9be7c55ee1baf692d8a8c55f69ebc
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 03:21:14 -0400

    feat: implement UI functionality to add contact details

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ContactForm from './ContactForm';
+import { addContact } from '../actions/contacts'
+
 
 class AddContact extends Component  {
-    render() {
+    render(props) {
         return (
             <div>
                 <h3>Add Contact</h3>
-                <ContactForm />
+                <ContactForm 
+                    onSubmit={ (contact) => {
+                        this.props.dispatch(addContact(contact));
+                    }}
+                />
             </div>
         );
     }
 }
 
-export default AddContact;
+export default connect()(AddContact);
