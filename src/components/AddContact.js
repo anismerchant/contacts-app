115810ab63731a2a5864c46038f4e56135ccbbf1
Anis Merchant <anisxmerchant@gmail.com>
2019-05-16 03:31:29 -0400

    refactor: convert 'AddContact' to a functional component

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import ContactForm from './ContactForm';
 import { addContact } from '../actions/contacts'
 
 
-class AddContact extends Component  {
-    render() {
-        return (
-            <div>
-                <h3>Add Contact</h3>
-                <ContactForm 
-                    onSubmit={ (contact) => {
-                        this.props.dispatch(addContact(contact));
-                        this.props.history.push('/dashboard');
-                    }}
-                />
-            </div>
-        );
-    }
+const AddContact = (props) => {
+    return (
+        <div>
+            <h3>Add Contact</h3>
+            <ContactForm 
+                onSubmit={ (contact) => {
+                    props.dispatch(addContact(contact));
+                    props.history.push('/dashboard');
+                }}
+            />
+        </div>
+    );
 }
 
 export default connect()(AddContact);
