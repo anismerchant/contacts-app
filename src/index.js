7e98dc411dd0203a13ffc6fcc7c9f30b42452fa2
Anis Merchant <anisxmerchant@gmail.com>
2019-05-17 14:36:41 -0400

    implement user authentication based on firebase user id

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import AppRouter from './routes/AppRouter';
+import AppRouter, { history } from './routes/AppRouter';
 import { beginSetContacts } from './actions/contacts';
+import { login, logout } from './actions/auth'
 import * as serviceWorker from './serviceWorker';
 import { firebase } from './firebase/firebase';
 import './index.css';
@@ -16,21 +17,33 @@ const jsx = (
     </Provider>
 );
 
+let renderOnce = false;
+const renderApp = () => {
+    if(!renderOnce) {
+        ReactDOM.render(jsx, document.getElementById('root'));
+        renderOnce = true;
+    }
+}
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
 
-store.dispatch(beginSetContacts()).then( () => {
-    ReactDOM.render(jsx, document.getElementById('root'));
+firebase.auth().onAuthStateChanged( (user) => {
+    if (user) {
+        store.dispatch(login(user.uid));
+        store.dispatch(beginSetContacts()).then( () => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
+    } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
 });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-firebase.auth().onAuthStateChanged( (user) => {
-    if (user) {
-        console.log('Logged In.')
-    } else {
-        console.log('Logged out')
-    }
-});
