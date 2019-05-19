53b89ffa2e505efc4a97c64d7166dc079cee5fa3
Anis Merchant <anisxmerchant@gmail.com>
2019-05-19 02:27:00 -0400

    feat: implement loading gif on delayed page loads

---
+++ src/components/LoadingGif.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingGif = () => (
+    <div className="loading-gif__section">
+       <img className="loading-gif__section--img" alt="loading-gif" src="/images/loading.gif" />
+    </div>
+);
+
+export default LoadingGif;
