34618fbd73243a9b970582118a28f8290463c8ec
Anis Merchant <anisxmerchant@gmail.com>
2019-05-19 02:22:33 -0400

    refactor: add header to 404 pages

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './Header';
 
 const PageNotFound = () => (
     <div className= "page-not-found__section">
+        <Header />
         <p className="page-not-found__section--msg">
             Oops, there's nothing here. Please try another page.
         </p>
