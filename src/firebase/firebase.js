849b49773ea32df61dab31f5ac6b1efcd5f84605
Anis Merchant <anisxmerchant@gmail.com>
2019-05-20 12:13:09 -0400

    fix: rename env variable as per Heroku docs

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import * as firebase from 'firebase';
 
 // Setup for deployment on Heroku
 const firebaseConfig = {
-  apiKey: JSON.stringify(process.env.FIREBASE_API_KEY),
-  authDomain: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-  databaseURL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-  projectId: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-  storageBucket: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-  messagingSenderId: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-  appId: JSON.stringify(process.env.FIREBASE_APP_ID)
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL:process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Gives us access to our database
