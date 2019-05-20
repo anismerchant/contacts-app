a5a9977669a6de96cd01e60b56a8d5c497d8ab6d
Anis Merchant <anisxmerchant@gmail.com>
2019-05-20 15:14:44 -0400

    style: add spacing between stats in contact list

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -7,7 +7,7 @@ import findContacts from '../selectors/findContacts';
 const ContactList = ({contacts}) => (
     <div className="contactlist__container">
         <div className="contact--stats">
-            {'You have a total'}<span className="contact--number"> {contacts.length} {'people'}</span>{' in your contact list.'}
+            {'You have a total of'}<span className="contact--number">&nbsp;{contacts.length} {'people'}&nbsp;</span>{' in your contact list.'}
         </div>
         <div className = "contact__cards">
             {contacts.map((contact) => <ContactListPerson key={contact.id} {...contact} />)}
